7b525b2d80eb3ccd0748b7022deb5c54fa45d207
osamunmun <osamu.takayasu@gmail.com>
2015-10-12 16:12:13 +0900

    Use template for offence.

--- base_hack/offence/views.py
+++ base_hack/offence/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from offence import scraping
 from django.http import HttpResponse
+from django.template.loader import render_to_string
 import json
 
 def batters(self):
     data = {'batters': scraping.scrape_batters()}
-    return HttpResponse(json.dumps(data, ensure_ascii=False),
-                                            content_type='application/json; charset=utf-8')
+    return HttpResponse(render_to_string('index.html', data))
+                                         # json.dumps(data, ensure_ascii=False),
+                                         #    content_type='application/json; charset=utf-8')
